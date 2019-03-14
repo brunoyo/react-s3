109fc529bfb8928f7d8270dc19aeb6c734db3827
brunoyo <bruno.younner@gmail.com>
2019-03-14 15:47:29 -0300

    update react

--- src/ReactS3.js
+++ src/ReactS3.js
@@ -6,11 +6,29 @@ import {
     dateYMD
 } from './Date'
 
-class ReactS3 {
-    static upload(file, config) {
+class S3FileUpload {
+    static async uploadFile(file, config) {
+
+
+        let envShort;
+        switch (process.env.NODE_ENV) {
+            case 'development' : 
+                envShort = 'dev'; break;
+            default:
+                envShort = 'dev'; break;
+        }
+        const filePath = envShort + `/${config.dirName}/` +
+                        (new Date()).getFullYear() + '-' +
+                        ((new Date()).getMonth() +1) + '/' +
+                        (new Date()).getTime() + '-' +
+                        (Math.random() + '').substr(2, 6) + '/';
+
         const fd = new FormData();
-        const key = `${config.albumName ? config.albumName + '/' : ''}${file.name}`;
-        const url = `https://${config.bucketName}.s3.amazonaws.com/`;
+        const fileName = file.name.replace(/\s/g, '');
+        const key = `${filePath}${fileName}`;
+        const url = `${config.bucketRoot}`;
+
+
         fd.append('key', key);
         fd.append('acl', 'public-read');
         fd.append('Content-Type', file.type);
@@ -23,61 +41,52 @@ class ReactS3 {
         fd.append('Policy', Policy.getPolicy(config))
         fd.append('X-Amz-Signature', Signature.getSignature(config, dateYMD, Policy.getPolicy(config)));
         fd.append("file", file);
-        return new Promise(async(resolve, reject) => {
-            const errorHandler = fn => {
-                return fn.then(data => [null, data])
-                    .catch(err => [err])
-            }
-            const uploadResult = async() => {
-                let err, result;
-                [err, result] = await errorHandler(fetch(url, {
-                    method: 'post',
-                    headers: {
-                        fd
-                    },
-                    body: fd
-                }))
-                if (err) reject(err)
-                resolve({
-                    bucket: config.bucketName,
-                    key: `${config.albumName ? config.albumName + '/' : ''}${file.name}`,
-                    location: `${url}${config.albumName ? config.albumName +'/' : ''}${file.name}`,
-                    result: result
-                })
-            }
-            uploadResult()
-        })
+        
+        const params = {
+            method: "post",
+            headers: {
+                fd
+            },
+            body: fd
+        };
+
+        const data = await fetch(url, params);
+        if (!data.ok) return Promise.reject(data);
+        return Promise.resolve({
+            bucket: config.bucketName,
+            key: `${filePath}${fileName}`,
+            location: `${url}${filePath}${fileName}`,
+            //key: `${config.dirName ? config.dirName + "/" : ""}${fileName}`,
+            //location: `${url}${filePath}${fileName}`,
+            result: data
+        });
     }
-    static delete(fileName, config) {
+    static async deleteFile(fileName, config) {
         const fd = new FormData();
-        const url = `https://${config.bucketName}.s3.amazonaws.com/${config.albumName ? config.albumName + '/' : ''}${fileName}`
-        fd.append('Date', xAmzDate)
-        fd.append('X-Amz-Date', xAmzDate);
-        fd.append('Authorization', Signature.getSignature(config, dateYMD, Policy.getPolicy(config)));
-        fd.append('Content-Type', 'text/plain')
-        return new Promise(async(resolve, reject) => {
-            const errorHandler = fn => {
-                return fn.then(data => [null, data])
-                    .catch(err => [err])
+        const url = `https://${config.bucketName}.s3-${config.region}.amazonaws.com/${config.dirName ? config.dirName + "/" : ""}${fileName}`;
+        fd.append("Date", xAmzDate);
+        fd.append("X-Amz-Date", xAmzDate);
+        fd.append("Authorization", Signature.getSignature(config, dateYMD, Policy.getPolicy(config)));
+        fd.append("Content-Type", "text/plain");
+
+        const params = {
+            method: "delete",
+            headers: {
+                fd
             }
-            const deleteResult = async() => {
-                let err, result;
-                [err, result] = await errorHandler(fetch(url, {
-                    method: 'delete',
-                    headers: {
-                        fd
-                    }
-                }))
-                if (err) reject(err)
-                resolve({
-                    ok: result.ok,
-                    status: result.status,
-                    message: 'File Deleted',
-                    fileName: fileName
-                })
-            }
-            deleteResult()
-        })
+        };
+
+        const deleteResult = await fetch(url, params);
+        if (!deleteResult.ok) return Promise.reject(deleteResult);
+        return Promise.resolve({
+            ok: deleteResult.ok,
+            status: deleteResult.status,
+            message: "File Deleted",
+            fileName: fileName
+        });
     }
 }
-export default ReactS3
+
+const { uploadFile, deleteFile } = S3FileUpload;
+export { uploadFile, deleteFile };
+export default S3FileUpload
